b017163995536a64ce94367eeccd625b8e7e2b2a
dresha-dev <ponamarevandrej@gmail.com>
2023-03-09 15:35:21 +0100

    fix: Domain

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -4,7 +4,7 @@ const config: GatsbyConfig = {
   pathPrefix: "/cv",
   siteMetadata: {
     title: `Andrii Ponamarov CV`,
-    siteUrl: `https://dresha-dev.github.io/cv`,
+    siteUrl: `https://dresha-dev.github.io`,
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
