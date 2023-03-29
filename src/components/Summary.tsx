5300cad1a73c4612b744aa7684063f389ee85d88
dresha-dev <ponamarevandrej@gmail.com>
2023-03-29 13:32:47 +0200

    fix: Typo in summary

--- src/components/Summary.tsx
+++ src/components/Summary.tsx
@@ -4,21 +4,19 @@ const Summary = () => (
   <section className="my-4 print:my-2 flex flex-col break-inside-avoid-page">
     <h2 className="mb-4 border-b print:text-sm print:md-2">Summary</h2>
     <p className="text-neutral-500">
-      Hi there👋
+      Hi there 👋
       <br />
-      Despite on my real name people call me "Dresha"😎. I'm a software engineer
-      with more than 11 years of professionally development outstanding products
-      🦄. Most of my career I was focusing on building frontend side of web
-      applications, but I also have experience in backend development and
+      Despite my real name people call me "Dresha". I'm a software engineer with
+      more than 11 years of professional development and outstanding products
+      🦄. For most of my career, I was focusing on building the frontend side of
+      web applications, but I also have experience in backend development and
       DevOps. I'm also a strong team player and I enjoy working with others to
       solve complex problems and build innovative solutions. I'm a lifelong
-      learner and I'm always looking for a new opportunities to grow and develop
-      my skills.
-      <br /> In my free time I'm interested in reading books, playing board
-      games, traveling, hiking, roller skating, yoga and basketball 🏀.
-      <br />
-      I'm currently interested in working remotely with React or Svelte
-      frameworks on frontend side and Node.js, NestJS on backend side.
+      learner and I'm always looking for new opportunities to grow and develop
+      my skills. In my free time, I'm interested in reading books, playing board
+      games, traveling, hiking, roller skating, yoga, and basketball. I'm
+      currently interested in working remotely with Reactor Svelte frameworks on
+      the front end side and Node.js, NestJs on the backend side.
     </p>
   </section>
 );
