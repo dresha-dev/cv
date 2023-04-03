5fec5702d2c150fdb6020d52e19d3ca4f01365ad
dresha-dev <ponamarevandrej@gmail.com>
2023-04-03 22:23:28 +0200

    feat: Add funny animations

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,6 +8,7 @@ import Languages from "../components/Languages";
 import Education from "../components/Education";
 import { Helmet } from "react-helmet";
 import Summary from "../components/Summary";
+import Cat from "../components/Cat";
 
 const IndexPage = () => (
   <main className="max-w-screen-lg mx-auto min-w-min print:overflow-visible p-4 print:m-0 print:p-0">
@@ -26,13 +27,13 @@ const IndexPage = () => (
     <div className="md:w-1/3 md:float-left print:float-left print:w-1/4">
       <Profile />
     </div>
-
     <Title className="pl-4 inline-block md:w-2/3 print:w-3/4 items-center md:items-start" />
 
     <div className="md:float-left print:float-left md:w-1/3 print:w-1/3 clear-left px-4 mt-2">
       <Skills />
       <Languages />
       <Education />
+      <Cat />
     </div>
     <div className="pl-4 inline-block md:w-2/3 print:w-2/3">
       <Summary />
